3552a17c027d490912b259f19bc3099a72895ea9
Sandro Sanchez <sandropucp@gmail.com>
2025-03-13 21:37:08 -0400

    adding login

---
+++ src/services/apiAuth.js
@@ -0,0 +1,16 @@
+
+export async function signup({ fullName, email, password }) {
+
+}
+
+export async function login({ email, password }) {
+}
+
+export async function getCurrentUser() {
+}
+
+export async function logout() {
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+}
